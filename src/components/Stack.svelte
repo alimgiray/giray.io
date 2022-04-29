d1a0311870bcfcf1a03788a790791d426feed8fa
Alim Giray Aytar <alimgiray@gmail.com>
2022-04-30 00:17:36 +0300

    make index page more generic

---
+++ src/components/Stack.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let stack;
+	export let first;
+	export let last;
+</script>
+
+{#if last}<span>&nbsp;and </span>{:else if !first}<span>, </span>{/if}
+<a href={stack.url} rel="external" target="_blank" class="whitespace-nowrap"
+	><img src={stack.logo} alt={stack.name} class="inline w-4 h-4 mr-1" />{stack.name}</a
+>{#if last}.{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,29 +2,25 @@
 	export const prerender = true;
 </script>
 
+<script>
+	import info from '../data/info.json';
+
+	import Stack from '../components/Stack.svelte';
+</script>
+
 <section class="mt-4 text-justify w-full md:w-1/3 mx-auto">
 	<h1 class="text-2xl text-center">
-		I'm <a href="/">Giray</a>, a full stack web developer from Turkey 🇹🇷
+		I'm <a href="/">{info.name}</a>, a {info.role} from {info.country}
 	</h1>
 	<p class="mt-8">
-		I build <a href="/work" class="underline">startups</a> but also worked for BigCorp<sup>TM</sup>
-		and created software with millions of users.
+		I build <a href="/work" class="underline">{info.work}</a>
+		{@html info.description}
 	</p>
 	<p class="mt-4">
 		My current stack is
-		<a href="https://go.dev/" rel="external" target="_blank" class="whitespace-nowrap"
-			><img src="go.svg" alt="golang" class="inline w-4 h-4 mr-1" />Golang</a
-		>,
-		<a href="https://nodejs.org/" rel="external" target="_blank" class="whitespace-nowrap"
-			><img src="node.svg" alt="node.js" class="inline w-4 h-4 mr-1" />Node</a
-		>,
-		<a href="https://vuejs.org/" rel="external" target="_blank" class="whitespace-nowrap"
-			><img src="vue.svg" alt="vue.js" class="inline w-4 h-4 mr-1" />Vue</a
-		>
-		and
-		<a href="https://svelte.dev/" rel="external" target="_blank" class="whitespace-nowrap"
-			><img src="svelte.svg" alt="svelte" class="inline w-4 h-4 mr-1" />Svelte</a
-		>.
+		{#each info.stack as stack, i}
+			<Stack {stack} first={i === 0} last={info.stack.length === i + 1} />
+		{/each}
 	</p>
 	<p class="mt-4">
 		Check out my <a href="projects" class="underline">projects</a> and
